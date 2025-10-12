165d3c04d9eba5a14c66bafb130f81329d061d34
Ryan <ryanic3@hotmail.co.nz>
2025-10-12 18:34:53 +1000

    finished the app (without cloud database only local storage)

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -11,12 +11,13 @@ import { ACCESS_TOKEN } from "./constants";
 // Default API URL used if no environment variable is provided
 const apiUrl = "/choreo-apis/awbo/backend/rest-api-be2/v1.0";
 
-// Create an Axios instance for making API requests
+// Create an Axios instance that handles all your HTTP requests.
 const api = axios.create({
   // Set the base URL: use the environment variable if it exists, otherwise use the default apiUrl
   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
 });
 
+// The interceptor adds your access token automatically before each request, so you don’t have to manually attach it.
 api.interceptors.request.use(
   (config) => {
     // Get the access token stored in the browser
